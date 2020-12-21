fe1d7c57de13c797f41c21e8e7dc471bf890534f
dekunukem <dekunukem@gmail.com>
2020-12-21 09:52:41 +0000

    updated commit scripts

--- push.sh
+++ push.sh
@@ -9,15 +9,7 @@ find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
-find ./pc_software -name "build" -exec rm -rf {} \;
-find ./pc_software -name "dist" -exec rm -rf {} \;
-find ./pc_software -name "*.zip" -exec rm -rf {} \;
-find ./pc_software -type f -name "*.spec*" -exec rm -f {} \;
 
-python update_firmware_ref.py
-rm ./sample_profiles.zip
-7z.exe a -r sample_profiles.zip ./sample_profiles
-zip -rv sample_profiles.zip ./sample_profiles
 git add --all
 git commit -m "$@"
 git push origin master
